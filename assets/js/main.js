b4bcfce5f3e437203512b5478987aa90a61e53d7
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-08-24 18:33:20 +0200

    fix the price calculator and add some ugly css

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,30 +7,31 @@ va applicato uno sconto del 40% per gli over 65.
  */
 
 
-
-//Il numero di chilometri da percorrere
-
-// età del passeggiero
-
-//calcolo il prezzo in base all'età
-
 let priceAge;
 
 document.getElementById('priceButton').addEventListener('click', function (e) {
 
    // console.log('ciao'); - debug
 
+   //Il numero di chilometri da percorrere
+
     const kmDOM = document.getElementById('km').valueAsNumber;
 
     console.log(kmDOM);
+
+    // età del passeggiero
 
     const ageUserDOM = document.getElementById('ageUser').value;
 
     console.log(ageUserDOM);
 
+    //prezzo temporaneo senza sconti
+
     const kmPrice = 0.21;
 
     let temporaryPrice = kmDOM * kmPrice;
+
+    //calcolo il prezzo in base all'età
 
     if (ageUserDOM == 'minorenne') {
 
@@ -47,9 +48,20 @@ document.getElementById('priceButton').addEventListener('click', function (e) {
     //stampo il prezzo e i dettagli
     const resultDOM = document.getElementById('priceResult');
 
-    const nomePasseggieroDOM = document.getElementById('userName').value;
-
-    console.log('nomePasseggieroDOM');
-
     resultDOM.innerHTML = 'il prezzo è ' + priceAge.toFixed(2) + ' €';
-});
+
+    const carrozzaDOM = document.getElementById('carrozza');
+
+    carrozzaDOM.innerHTML = (Math.random()*20).toFixed();
+
+    //passeggiero nome
+
+    const userDOM = document.getElementById('username').value;
+
+    console.log(userDOM);
+
+    const nomePasseggieroDOM = document.getElementById('nomePasseggiero');
+
+    nomePasseggieroDOM.innerHTML = userDOM;
+
+}); 
